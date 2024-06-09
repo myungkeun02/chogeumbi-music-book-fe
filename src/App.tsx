0347bbdd0a11af9025414f724aca6a5bb1fc67cf
Hyeon Woo Jo <hyeonwoo5342@gmail.com>
2024-06-09 15:16:36 +0900

    feat: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Component } from "./components/component";
+
+function App() {
+  return (
+    <>
+      <Component />
+      {/* <header className="font-lee-seo-yun my-5">
+        <div className="container max-w-3xl border border-gray-200 p-4 rounded-2xl">
+          <h1>초금비 노래책</h1>
+        </div>
+      </header> */}
+    </>
+  );
+}
+
+export default App;
